74986591fca901025e4d190b90febd1227a30b36
abanam <abanam275@gmail.com>
2025-04-14 22:10:32 +0600

    toggle page  button done

---
+++ index.js
@@ -0,0 +1,27 @@
+const goButton = document.getElementById('goButton');
+if (goButton) {
+  goButton.addEventListener('click', function () {
+    window.location.href = './blog.html';
+  });
+}
+
+const backButton = document.getElementById('backButton');
+if (backButton) {
+  backButton.addEventListener('click', function () {
+    window.location.href = './index.html';
+  });
+}
+
+const buttons = document.querySelectorAll('.toggle-btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => {
+      btn.classList.remove('bg-barn-primary');
+      btn.classList.add('bg-gray-200');
+    });
+
+    button.classList.remove('bg-gray-200');
+    button.classList.add('bg-barn-primary');
+  });
+});
